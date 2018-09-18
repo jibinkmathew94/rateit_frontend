44b8c6504d5be984e7d4364be5b3bc0a28f486d7
Jibin Kavumpurath Mathew <jibinkmathew@Jibins-MacBook-Pro.local>
2018-09-18 14:19:51 +0530

    Configured webpack and react

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,8 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
 module.exports = {
   module: {
     rules: [
@@ -9,5 +14,6 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [htmlPlugin]
 };
